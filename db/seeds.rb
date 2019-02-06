be45ffb2c692b9cd9f9de453b1e3dda32b1aa547
VictorArd <victor.airiaud@outlook.fr>
2019-02-06 15:44:05 +0100

    First Commit

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,23 +8,22 @@
 
 require 'faker'
 
-2.times do
+10.times do
   @c = City.create(name: Faker::Address.city, zip_code: Faker::Address.zip)
-
 end
 p "Création de villes"
 
-5.times do
+10.times do
   @u = User.create(f_name: Faker::Name.first_name, l_name: Faker::Name.middle_name, descrip: Faker::Music.album, email: Faker::Internet.email, age: Faker::Date.birthday(min_age = 18, max_age = 65), city: @c)
 end
 p "Création de users"
 
-3.times do
+10.times do
   @p = Potin.create(title: Faker::Job.title, content: Faker::Pokemon.name, user: @u)
 end
 p "Création de potins"
 
-2.times do
+10.times do
   t = Tag.create(title: Faker::Job.title, potin: @p)
 end
 p "Création de tags"
