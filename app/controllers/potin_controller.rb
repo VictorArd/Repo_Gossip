e3f79680bc6f8661245078b781b6481378f3b3d5
VictorArd <victor.airiaud@outlook.fr>
2019-02-06 18:27:28 +0100

    Add forms

--- app/controllers/potin_controller.rb
+++ app/controllers/potin_controller.rb
@@ -12,12 +12,16 @@ class PotinController < ApplicationController
   end
 
   def create
-    @potin = Potin.new() # avec xxx qui sont les données obtenues à partir du formulaire
-  
-    if @gossip.save # essaie de sauvegarder en base @gossip
+    puts '$' * 60
+    puts params
+    @users = User.all
+    @potin = Potin.new(title: params[:titre], content: params[:content], user: User.find(params[:auteur]))
+    if @potin.save # essaie de sauvegarder en base @gossip
       # si ça marche, il redirige vers la page d'index du site
+      redirect_to :accueil
     else
       # sinon, il render la view new (qui est celle sur laquelle on est déjà)
+      render :new
     end
   end
 end
